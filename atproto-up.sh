a7ad32d4816bae972d4affeacacbfd03a5c4bbb0
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-09-18 23:59:25 +0100

    Add Tailscale service and configuration to Docker setup
    - Introduced .env.example for Tailscale client credentials
    - Updated .gitignore to exclude .env files
    - Modified docker-compose.yml to include Tailscale service with necessary environment variables
    - Created nginx.conf for handling SSL and proxying requests to services

--- atproto-up.sh
+++ atproto-up.sh
@@ -4,30 +4,34 @@ set -ex
 
 git submodule update --init --recursive
 
-docker compose up -d
+docker compose up -d tailscale
 
-postgres_container=$(docker ps --filter "name=postgres" --format '{{.Names}}')
-plc_container=$(docker ps --filter "name=plc" --format '{{.Names}}')
-pds_container=$(docker ps --filter "name=pds.internal" --format '{{.Names}}')
-relay_container=$(docker ps --filter "name=relay" --format '{{.Names}}')
+sleep 5
 
-until [ "$(docker inspect -f '{{.State.Health.Status}}' ${postgres_container})" = "healthy" ] && \
-      [ "$(docker inspect -f '{{.State.Health.Status}}' ${plc_container})" = "healthy" ] && \
-      [ "$(docker inspect -f '{{.State.Health.Status}}' ${pds_container})" = "healthy" ] && \
-      [ "$(docker inspect -f '{{.State.Health.Status}}' ${relay_container})" = "healthy" ]; do sleep 2; done
+docker compose exec tailscale /bin/sh -c "tailscale cert --cert-file /mnt/tls/cert.pem --key-file /mnt/tls/cert.key atproto-test.tail4d0c1f.ts.net"
 
-# Hacking around some security checks in the relay when adding a host
-echo "Adding pds.internal to relay"
-docker exec -e PGPASSWORD=postgres  ${postgres_container} psql -h postgres -U postgres -d relay \
-    -c "INSERT INTO host (hostname, no_ssl, account_limit, trusted, status, last_seq, account_count, created_at, updated_at) \
-            VALUES ('pds.internal', true, 1000, true, 'active', -1, 0, NOW(), NOW()) \
-            ON CONFLICT (hostname) DO NOTHING;"
+# postgres_container=$(docker ps --filter "name=postgres" --format '{{.Names}}')
+# plc_container=$(docker ps --filter "name=plc" --format '{{.Names}}')
+# pds_container=$(docker ps --filter "name=pds.internal" --format '{{.Names}}')
+# relay_container=$(docker ps --filter "name=relay" --format '{{.Names}}')
 
-docker compose restart relay
+# until [ "$(docker inspect -f '{{.State.Health.Status}}' ${postgres_container})" = "healthy" ] && \
+#       [ "$(docker inspect -f '{{.State.Health.Status}}' ${plc_container})" = "healthy" ] && \
+#       [ "$(docker inspect -f '{{.State.Health.Status}}' ${pds_container})" = "healthy" ] && \
+#       [ "$(docker inspect -f '{{.State.Health.Status}}' ${relay_container})" = "healthy" ]; do sleep 2; done
 
-echo "Service ports:"
-docker compose ps --format "table {{.Name}}\t{{.Service}}\t{{.Publishers}}"
+# # Hacking around some security checks in the relay when adding a host
+# echo "Adding pds.internal to relay"
+# docker exec -e PGPASSWORD=postgres  ${postgres_container} psql -h postgres -U postgres -d relay \
+#     -c "INSERT INTO host (hostname, no_ssl, account_limit, trusted, status, last_seq, account_count, created_at, updated_at) \
+#             VALUES ('pds.internal', true, 1000, true, 'active', -1, 0, NOW(), NOW()) \
+#             ON CONFLICT (hostname) DO NOTHING;"
 
-read -p "Press enter to run PDS+relay tests"
+# docker compose restart relay
 
-node ./test_pds_relay.mjs
+# echo "Service ports:"
+# docker compose ps --format "table {{.Name}}\t{{.Service}}\t{{.Publishers}}"
+
+# read -p "Press enter to run PDS+relay tests"
+
+# node ./test_pds_relay.mjs
