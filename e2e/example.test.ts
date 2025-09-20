7e6c1a094e413a419577516954e35cf30702e726
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-09-20 23:06:11 +0100

    Add E2E test workflow and update environment variable usage for Tailscale

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -1,15 +1,17 @@
 import { AtpAgent } from "@atproto/api";
 import { Firehose } from "@skyware/firehose";
 
-const TS_DOMAIN = process.env.TS_DOMAIN;
-if (!TS_DOMAIN) {
-  throw new Error("TS_DOMAIN env var is required");
+const TAILSCALE_DOMAIN = process.env.TAILSCALE_DOMAIN;
+if (!TAILSCALE_DOMAIN) {
+  throw new Error("TAILSCALE_DOMAIN env var is required");
 }
 
 describe("Local ATProto E2E Tests", () => {
   it("commits on a PDS are pushed on a relay", async () => {
     const commitEvents = [];
-    const firehose = new Firehose({ relay: `https://relay.${TS_DOMAIN}` });
+    const firehose = new Firehose({
+      relay: `https://relay.${TAILSCALE_DOMAIN}`,
+    });
 
     firehose.on("commit", (commit) => {
       commitEvents.push(commit);
@@ -18,7 +20,7 @@ describe("Local ATProto E2E Tests", () => {
     firehose.start();
 
     const agent = new AtpAgent({
-      service: `https://pds.${TS_DOMAIN}`,
+      service: `https://pds.${TAILSCALE_DOMAIN}`,
     });
 
     const rand = Math.floor(Math.random() * 10000);
@@ -26,7 +28,7 @@ describe("Local ATProto E2E Tests", () => {
     await agent.createAccount({
       email: `${name}@mail.com`,
       password: "abc123",
-      handle: `${name}.pds.${TS_DOMAIN}`,
+      handle: `${name}.pds.${TAILSCALE_DOMAIN}`,
     });
 
     const test = "Hello world!";
